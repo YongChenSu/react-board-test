0ced1babb17779553b9099bb44fec511aa50398b
MSI\USER <yongchensu0@gmail.com>
2020-11-04 16:48:40 +0800

    Update App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+
+import { useState, useRef } from "react";
+import TodoItem from "./TodoItem";
 
 function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, content: "done", isDone: true },
+    { id: 2, content: "not done", isDone: false },
+  ]);
+  const [value, setValue] = useState("");
+  const id = useRef(3);
+
+  const handleButtonClick = () => {
+    setTodos([
+      {
+        id: id.current,
+        content: value,
+      },
+      ...todos,
+    ]);
+    setValue("");
+    id.current++;
+  };
+
+  const handleInputChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleToggleIsDone = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id !== id) return todo;
+        return {
+          ...todo,
+          isDone: !todo.isDone,
+        };
+      })
+    );
+  };
+
+  const handleDeleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* ref 存取 DOM 元素 */}
+      <input
+        type="text"
+        placeholder="todo"
+        value={value}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleButtonClick}>Add Todo</button>
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          handleToggleIsDone={handleToggleIsDone}
+          handleDeleteTodo={handleDeleteTodo}
+        />
+      ))}
     </div>
   );
 }
